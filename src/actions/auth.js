9faf62afa63f995e063fe5a4e5d68d699b2797fc
Adam Goldschmidt <adamgold7@gmail.com>
2019-01-30 13:08:54 +0200

    display different screens for teacher, student or normal user

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -109,21 +109,46 @@ export const openFacebook = (token = "") => {
 	Linking.openURL(url)
 }
 
-/*export const registerDeviceToken = () => {
-	return async (dispatch, getState) => {
-		fcmToken = await firebase.messaging().getToken()
-		if (fcmToken) {
-			const { user, fetchService } = getState()
-			resp = await fetchService.fetch("/login/register_firebase_token", {
-				method: "POST",
-				body: JSON.stringify({
-					firebase_token: fcmToken
-				})
+/*
+export const registerDeviceToken = async state => {
+	let existing_token = await Storage.getItem("firebase_token", true)
+	if (existing_token) {
+		// we already registered the firebase token.
+		// let's check it's expiry and if it's expired,
+		// register again
+		existing_token = JSON.parse(existing_token)
+		if (new Date() >= existing_token.expiry) {
+			// expired
+			_registerDeviceToken(state)
+		}
+		return
+	}
+	_registerDeviceToken(state)
+}
+
+const _registerDeviceToken = async state => {
+	fcmToken = await firebase.messaging().getToken()
+	if (fcmToken) {
+		const { user, fetchService } = state
+		resp = await fetchService.fetch("/api/register_token", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				firebase_token: fcmToken
 			})
-			if (!("user_id" in resp.json)) return
-			if (resp.json.user_id == user.id && resp.status == 201) {
-				dispatch({ type: "DEVICE_TOKEN_ADDED" })
-			}
+		})
+		if (!("user_id" in resp.json)) return
+		if (resp.json.user_id == user.id && resp.status == 201) {
+			date = new Date()
+			expiry = date.setDate(date.getDate() + 7) // 7 days from now
+			Storage.setItem(
+				"firebase_token",
+				JSON.stringify({ token: fcmToken, expiry }),
+				true
+			)
 		}
 	}
-} */
+}
+*/

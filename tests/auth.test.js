85e302a4061943df2c0c86ebb20c3baa62c9ee53
Adam Goldschmidt <adamgold7@gmail.com>
2019-02-03 18:10:31 +0200

    configure jest

---
+++ tests/auth.test.js
@@ -0,0 +1,8 @@
+import React from "react"
+import AuthLoading from "../src/screens/auth/AuthLoading"
+import renderer from "react-test-renderer"
+
+test("AuthLoading renders correctly", () => {
+	const tree = renderer.create(<AuthLoading />).toJSON()
+	expect(tree).toMatchSnapshot()
+})

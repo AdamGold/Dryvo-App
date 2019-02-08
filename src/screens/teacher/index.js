1c1ab793020c1ec5aecfce44f4f1df4b79857bce
Adam Goldschmidt <adamgold7@gmail.com>
2019-02-08 19:15:14 +0200

    add noitication screen

--- src/screens/teacher/index.js
+++ src/screens/teacher/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react"
 import { createBottomTabNavigator } from "react-navigation"
 import Home from "./Home"
+import Notifications from "./Notifications"
 
 export default createBottomTabNavigator({
-	Home: Home
+	Home: Home,
+	Notifications: Notifications
 })

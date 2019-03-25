01fa7234f95150af11c89f79353b32447a993d36
Adam Goldschmidt <adamgold7@gmail.com>
2019-03-25 15:18:40 +0200

    add empty topics image

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -7,7 +7,8 @@ export default class EmptyState extends React.Component {
 			lessons: require("../../assets/images/lessons.png"),
 			payments: require("../../assets/images/payments.png"),
 			notifications: require("../../assets/images/notifications.png"),
-			students: require("../../assets/images/students.png")
+			students: require("../../assets/images/students.png"),
+			topics: require("../../assets/images/topics.png")
 		}
 		return (
 			<View style={{ ...styles.container, ...this.props.style }}>

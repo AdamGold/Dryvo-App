a4d2e9299268d1e49bd5f4c5130f04a5c23c517a
Adam Goldschmidt <adamgold7@gmail.com>
2019-04-05 19:24:45 +0300

    fix #2 for home current lesson bug

--- src/screens/teacher/Home.js
+++ src/screens/teacher/Home.js
@@ -85,16 +85,22 @@ export class Home extends React.Component {
 			"/lessons/?limit=2&is_approved=true&date=ge:" + now,
 			{ method: "GET" }
 		)
-		let nextLesson = nextLessonResp.json["data"][1]
-		let currentLesson = {}
-		if (currentLessonResp.json["data"].length == 0) {
-			// no current lesson (no lessons today)
-			nextLesson = nextLessonResp.json["data"][0]
+		let nextLesson = null,
+			currentLesson = null
+
+		if (nextLessonResp.json["data"].length > 0) {
+			nextLesson = nextLessonResp.json["data"][1]
 		} else {
-			currentLesson = currentLessonResp.json["data"][0]
+			if (currentLessonResp.json["data"].length == 0) {
+				// no current lesson (no lessons today)
+				nextLesson = nextLessonResp.json["data"][0]
+			} else {
+				currentLesson = currentLessonResp.json["data"][0]
+			}
 		}
+
 		this.setState({
-			currentLesson: currentLesson,
+			currentLesson,
 			nextLesson
 		})
 	}
@@ -119,8 +125,7 @@ export class Home extends React.Component {
 	}
 
 	renderLesson = item => {
-		console.log(item)
-		if (item.length == 0) {
+		if (!item) {
 			return (
 				<Text
 					style={{

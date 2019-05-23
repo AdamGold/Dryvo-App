d254034a2bf3564f804b8c25ee05a2d9c57c816c
Adam Goldschmidt <adamgold7@gmail.com>
2019-05-23 12:15:05 +0300

    adjust tests after RN 0.59 upgrade

--- tests/components/Logo.test.js
+++ tests/components/Logo.test.js
@@ -4,6 +4,7 @@ import renderer from "react-test-renderer"
 
 import Logo from "../../src/components/Logo"
 
+jest.useFakeTimers()
 describe("Logo", () => {
 	test("view renders correctly", () => {
 		const tree = renderer.create(<Logo size="large" />).toJSON()

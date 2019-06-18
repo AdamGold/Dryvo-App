a99d45510ab2ed11dc29dca3dc23e776aaa3ead1
Adam Goldschmidt <adamgold7@gmail.com>
2019-06-18 17:49:38 +0300

    update snapshots

--- src/components/AlertError.js
+++ src/components/AlertError.js
@@ -6,7 +6,6 @@ import { getError } from "../actions/utils"
 export default class AlertError extends React.Component {
 	componentDidUpdate() {
 		const error = this.props.dispatch(getError())
-		console.log(error)
 		if (error) {
 			Alert.alert(strings("errors.title"), errors(error))
 		}
